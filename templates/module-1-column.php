3e1cb6bab419700a9e7faf42856e62b594da9e1d
joeycrowle <joeycrowle@gmail.com>
2018-04-16 00:17:02 -0700

    aux page / archive styles

---
+++ templates/module-1-column.php
@@ -0,0 +1,11 @@
+<?php
+$col1 = get_sub_field('column_1');
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <?php echo $col1 ?>
+    </div>
+  </div>
+</div>
---
+++ templates/module-2-column.php
@@ -0,0 +1,15 @@
+<?php
+$col1 = get_sub_field('column_1');
+$col2 = get_sub_field('column_2');
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+      <?php echo $col1 ?>
+    </div>
+    <div class="col-sm-6">
+      <?php echo $col2 ?>
+    </div>
+  </div>
+</div>
